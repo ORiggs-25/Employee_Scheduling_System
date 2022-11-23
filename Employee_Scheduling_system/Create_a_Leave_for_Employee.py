6e424579dcf6619c045aa67c8502fbf1e82244eb
Joshua Furman <jafbird52@gmail.com>
2022-11-23 11:57:02 -0800

    Created Create_A_Leave_for_Employee.py

---
+++ Employee_Scheduling_system/Create_a_Leave_for_Employee.py
@@ -0,0 +1,85 @@
+import json
+import tkinter as tk  # GUI module
+import requests
+from tkinter import Canvas, LEFT
+from PIL import ImageTk  # Display background
+
+
+def main():
+    # -----following code pertains to main user input window------------------------------------------------------------
+    # creating object from Tkinter module
+    root = tk.Tk()
+    # renames the title of the window
+    root.title("Create Employee Leave")
+    # sets the dimensions of the window to measurement
+    root.geometry('1200x800')
+    # prevents user to resize the window
+    root.resizable(width=False, height=False)
+
+    # canvas function will create a background for the GUI
+    my_canvas: Canvas = tk.Canvas(root, width=1200, height=800, bd=0, highlightthickness=0)
+    my_canvas.pack(fill="both", expand=True)
+
+    # using pillow's ImageTk class and PhotoImage function to display background photo
+    bg = ImageTk.PhotoImage(file="BasicBlue.jpeg")
+    my_canvas.create_image(0, 0, image=bg, anchor="nw")
+
+    # ===============================================================================================================
+
+    def create_Leave(id, leaveType, startDate, endDate):
+
+        headers = {"Content-Type": "application/json",
+                   "Connection": "keep-alive"}
+
+        createLeave = {
+            "employeeID": str(id),
+            "leave_type": str(leaveType),
+            "start_date": str(startDate),
+            "end_date": str(endDate)
+        }
+
+        try:
+            response = requests.put("https://uhwxroslh0.execute-api.us-east-1.amazonaws.com/dev/create/leave",
+                                    data=json.dumps(createLeave), headers=headers)
+            print(response.status_code)
+
+            my_canvas.create_text(350, 370, text="Leave Was Successfully Added.", font=("Helvetica", 16), fill="white")
+        except requests.exceptions.HTTPError as err:
+            print(err)
+
+    # ==================================================================================================================
+
+    # create_text function from tkinter will display text onto GUI
+    my_canvas.create_text(575, 50, text="Create A Leave (Type Exactly As Shown)", font=("Helvetica", 21), fill="white")
+    my_canvas.create_text(300, 140, text="EmployeeID", font=("Helvetica", 16), fill="white")
+    my_canvas.create_text(300, 190, text="Type: (paid/unpaid)", font=("Helvetica", 16), fill="white")
+    my_canvas.create_text(300, 240, text="Start: (mm/dd/yyyy)", font=("Helvetica", 16), fill="white")
+    my_canvas.create_text(300, 290, text="End: (mm/dd/yyyy)", font=("Helvetica", 16), fill="white")
+
+    # create Entry text boxes
+    employeeID = tk.Entry(my_canvas, font=("Helvetica", 12), width=50, bg="white", borderwidth=2)
+    employeeID.pack()
+    leaveType = tk.Entry(my_canvas, font=("Helvetica", 12), width=50, bg="white", borderwidth=2)
+    leaveType.pack()
+    startDate = tk.Entry(my_canvas, font=("Helvetica", 12), width=50, bg="white", borderwidth=2)
+    startDate.pack()
+    endDate = tk.Entry(my_canvas, font=("Helvetica", 12), width=50, bg="white", borderwidth=2)
+    endDate.pack()
+
+    employeeID_window = my_canvas.create_window(375, 125, anchor="nw", window=employeeID)
+    leaveType_window = my_canvas.create_window(375, 175, anchor="nw", window=leaveType)
+    startDate_window = my_canvas.create_window(375, 225, anchor="nw", window=startDate)
+    endDate_window = my_canvas.create_window(375, 275, anchor="nw", window=endDate)
+
+    create_button = tk.Button(root, text="Create Leave", activeforeground='blue', font=("Helvetica", 15),
+                              width=15, height=20, borderwidth=2,
+                              command=lambda: create_Leave(employeeID.get(), leaveType.get(), startDate.get(),
+                                                           endDate.get()))
+
+    create_button_window = my_canvas.create_window(500, 325, height=35, anchor="nw", window=create_button)
+
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
--- Employee_Scheduling_system/Display_All_Employees.py
+++ Employee_Scheduling_system/Display_All_Employees.py
@@ -1,163 +1,59 @@
-import tkinter
-from tkinter.constants import LEFT
-from tkinter.ttk import Label
+from tkinter import *
+from tkinter import ttk
 
 import requests
-import json
-
-import tkinter as tk  # GUI module
-from PIL import ImageTk  # Display background
-import tkinter.font as font
 
 
-def callDisplaySingleEmployee():
-    # Want this class to be called upon pressing the button at the bottom of the ViewAllEmployees screen
-    print("Moving To Search Specific Employee...")
-    # Want this to calls the "Display_Single_Employee" class
+# Create an instance of tkinter frame
+win = Tk()
 
-    pass
+# Set the size of the tkinter window
+win.geometry("1200x800")
 
+# Create an object of Style widget
+style = ttk.Style()
+style.theme_use('clam')
+# Configure the style of Heading in Treeview widget
+style.configure('Treeview.Heading', background="blue")
 
-# Creates a canvas window to display FirstName and LastName of all employees with button at the bottom
-# that calls other class.
-# We want "Display_All_Employees" to path to "Display_Single_Employee"
-def viewAllEmployees():
-    headers = {
-        "Content-Type": "application/json",
-        "Connection": "keep-alive",
-    }
+headers = {"Content-Type": "application/json",
+                   "Connection": "keep-alive"}
 
-    try:
+try:
+    response = requests.get("https://uhwxroslh0.execute-api.us-east-1.amazonaws.com/dev/employees")
+    jsonData = response.json()["Items"]
 
-        # Opens a new canvas that displays all employees in the database
-        root = tk.Tk()
-        # renames the title of the window
-        root.title("Admin Portal")
-        # sets the dimensions of the window to measurement
-        root.geometry('1200x800')
-        # prevents user to resize the window
-        root.resizable(width=False, height=False)
-
-        # canvas function will create a background for the GUI
-        my_canvas = tk.Canvas(root, width=400, height=200, bd=0, highlightthickness=0)
-        my_canvas.configure(bg="light blue")
-        my_canvas.pack(fill="both", expand=True)
+    tree = ttk.Treeview(win, column=("Employee ID","First Name", "Last Name", "Status", "Role ID"), show='headings', height=100)
+    tree.column("# 1", anchor=CENTER)
+    tree.heading("# 1", text="Employee ID")
+    tree.column("# 2", anchor=CENTER)
+    tree.heading("# 2", text="First Name")
+    tree.column("# 3", anchor=CENTER)
+    tree.heading("# 3", text="Last Name")
+    tree.column("# 4", anchor=CENTER)
+    tree.heading("# 4", text="Status")
+    tree.column("# 5", anchor=CENTER)
+    tree.heading("# 5", text="Role ID")
 
 
-        # Creates a button at the bottom of the screen to direct user to "Display single employee"
-        create_button = tk.Button(root, text="Search Specific Employee", activeforeground="blue", font=("Helvetica", 10)
-                                  , width=30, height=20, borderwidth=2, command=lambda: callDisplaySingleEmployee())
-        my_canvas.create_window(500, 750, height=30, anchor="nw", window=create_button)
+    for data in jsonData:
+        if data["roleID"] == 1:
+            data["roleID"] = "Intern"
+        elif data["roleID"] == 2:
+            data["roleID"] = "Associate"
+        elif data["roleID"] == 3:
+            data["roleID"] = "Supervisor"
+        elif data["roleID"] == 4:
+            data["roleID"] = "Manager"
+        if data["roleID"] == 5:
+            data["roleID"] = "Executive"
 
-        # Pulls data from server
-        request = requests.get("https://uhwxroslh0.execute-api.us-east-1.amazonaws.com/dev/employees")
-        employeesFromResponse = request.json()["Items"]
+        # Insert the data in Treeview widget
+        tree.insert('', 'end', text="1", values=(data['id'], data['firstName'], data['lastName'], data['status'], data['roleID']))
+    tree.pack()
 
-        # Instantiates where to place the text in window
-        x = -100
-        y = 30
+except requests.exceptions.HTTPError as err:
+    print(err)
+# Add a Treeview widget
 
-        # Loop for fetching each employee information
-        for emp in employeesFromResponse:
-
-            # This is for when the employees reach the end of the screen it goes to the next line
-            if x > 1000:
-                x = -100
-                y += 200
-
-            # Gathers all info, can copy this code and use for specific employee detail in
-            # "Display_Single_Employee"
-            lastN = emp["lastName"]
-            birthday = emp["dob"]
-            status = emp["status"]
-            address = emp["address"]
-            id = emp["id"]
-            email = emp["email"]
-            phone = emp["phone"]
-            firstN = emp["firstName"]
-            roleId = emp["roleID"]
-
-            # Offset for next employee so data and listing is not stacked
-            x += 150
-
-            # Loop for displaying each employee's specific information
-            for theEmp in range(3):
-                my_canvas.create_text(x, y - 20, text="Employee ID: " + str(id), font=("Helvetica", 10), fill="black")
-                # Take out commented parts for "Display_Single_Employee" to add in more specific data
-                if theEmp == 0:
-                    my_canvas.create_text(x, y, text="Last Name: " + lastN, font=("Helvetica", 10), fill="black")
-                    """
-                elif theEmp == 2:
-                    my_canvas.create_text(x, y - 20, text="Birthday: " + birthday, font=("Helvetica", 10), fill="black")
-                elif theEmp == 3:
-                    my_canvas.create_text(x, y - 40, text="Status : " + status, font=("Helvetica", 10), fill="black")
-                elif theEmp == 4:
-                    my_canvas.create_text(x, y - 60, text="Address: " + address, font=("Helvetica", 10), fill="black")
-                elif theEmp == 5:
-                    my_canvas.create_text(x, y - 80, text="ID: " + str(id), font=("Helvetica", 10), fill="black")
-                elif theEmp == 6:
-                    my_canvas.create_text(x, y - 100, text="Email: " + email, font=("Helvetica", 10), fill="black")
-                elif theEmp == 7:
-                    my_canvas.create_text(x, y - 120, text="Phone: " + phone, font=("Helvetica", 10), fill="black")
-                    """
-                elif theEmp == 1:
-                    my_canvas.create_text(x, y - 10, text="First Name: " + firstN, font=("Helvetica", 10),
-                                          fill="black")
-
-            # elif theEmp == 9:
-            #  my_canvas.create_text(x, y - 160, text="Role ID: " + str(roleId), font=("Helvetica", 10),
-            #                        fill="black")
-    except requests.exceptions.HTTPError as err:
-        print(err)
-
-
-"""
-            my_canvas.create_text(600, 500, text="Last Name: " + lastN, font=("Helvetica", 10), fill="black")
-            my_canvas.create_text(600, 450, text="Birthday: " + birthday, font=("Helvetica", 10), fill="black")
-            my_canvas.create_text(600, 400, text="Status : " + status, font=("Helvetica", 10), fill="black")
-            my_canvas.create_text(600, 350, text="Address: " + address, font=("Helvetica", 10), fill="black")
-            my_canvas.create_text(600, 300, text="ID: " + str(id), font=("Helvetica", 10), fill="black")
-            my_canvas.create_text(600, 250, text="Email: " + email, font=("Helvetica", 10), fill="black")
-            my_canvas.create_text(600, 200, text="Phone: " + phone, font=("Helvetica", 10), fill="black")
-            my_canvas.create_text(600, 150, text="First Name: " + firstN, font=("Helvetica", 10), fill="black")
-            my_canvas.create_text(600, 100, text="Role ID: " + str(roleId), font=("Helvetica", 20), fill="black")
-"""
-
-
-def main():
-    # -----following code pertains to main user input
-    # window--------------------------------------------------------------- creating object from Tkinter module
-    root = tk.Tk()
-    # renames the title of the window
-    root.title("Admin Portal")
-    # sets the dimensions of the window to measurement
-    root.geometry('1200x800')
-    # prevents user to resize the window
-    root.resizable(width=False, height=False)
-
-    # canvas function will create a background for the GUI
-    my_canvas = tk.Canvas(root, width=800, height=400, bd=0, highlightthickness=0)
-    my_canvas.pack(fill="both", expand=True)
-
-    # using pillow's ImageTk class and PhotoImage function to display background photo
-    bg = ImageTk.PhotoImage(file="addEmployee1.jpg")
-    my_canvas.create_image(0, 0, image=bg, anchor="nw")
-
-    # create_text function from tkinter will display text onto GUI
-    my_canvas.create_text(600, 140, text="View All Employees?", font=("Helvetica", 40), fill="white")
-
-    # We used the Button function in Tkinter which will call the function once clicked
-    create_button = tk.Button(root, text="View", activeforeground='white', font=("Helvetica", 30),
-                              width=15, height=20, borderwidth=2, command=lambda: viewAllEmployees())
-
-    my_canvas.create_window(450, 200, height=35, anchor="nw", window=create_button)
-
-    root.mainloop()
-
-
-if __name__ == '__main__':
-    main()
-
-"""
-
-"""
+win.mainloop()
